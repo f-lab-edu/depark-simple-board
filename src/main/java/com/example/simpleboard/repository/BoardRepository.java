eec9208806569fe87f6f28c03b1519a398c97b81
depark <mem29238@gmail.com>
2024-08-20 00:26:08 +0900

    feat: Add pessmistic write lock

--- src/main/java/com/example/simpleboard/repository/BoardRepository.java
+++ src/main/java/com/example/simpleboard/repository/BoardRepository.java
@@ -1,7 +1,14 @@
 package com.example.simpleboard.repository;
 
 import com.example.simpleboard.entity.Board;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Board> findById(Long id);
 }
